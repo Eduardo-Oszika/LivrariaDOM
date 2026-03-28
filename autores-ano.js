f08161e31a97c3d2df4311d456d6e97b2fe39f28
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2026-03-28 01:10:00 -0300

    pagina html com os autores e anos dos livros

---
+++ autores-ano.js
@@ -0,0 +1,21 @@
+var livrosXML = new XMLHttpRequest();
+livrosXML.open("GET", "livros.xml", false);
+livrosXML.send();
+
+var DOM = livrosXML.responseXML;
+var autores = DOM.getElementsByTagName("autor");
+var anos = DOM.getElementsByTagName("ano");
+
+var divAutoresAno = document.getElementById("autores-anos");
+
+for (var i = 0; i < autores.length; i++) {
+    var autor = autores[i].childNodes[0].nodeValue;
+    var ano = anos[i].childNodes[0].nodeValue;
+
+    var p = document.createElement("p");
+
+    var texto = "Autor: " + autor + ", Ano: " + ano;
+
+    p.textContent = texto;
+    divAutoresAno.appendChild(p);
+}
