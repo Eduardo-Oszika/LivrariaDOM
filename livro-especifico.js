12fb658376dd597b37a4fbf4b7f22dc5f2f917b3
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2026-03-28 01:21:46 -0300

    adiciona a página HTML e o script para exibição de um livro específico

---
+++ livro-especifico.js
@@ -0,0 +1,23 @@
+
+var livrosXML = new XMLHttpRequest();
+livrosXML.open("GET", "livros.xml", false);
+livrosXML.send();
+
+var DOM = livrosXML.responseXML;
+
+var livro = DOM.getElementsByTagName('livro')[0];
+
+var id = livro.getAttribute('id');
+
+var titulo = livro.getElementsByTagName('titulo')[0];
+var autor = livro.getElementsByTagName('autor')[0];
+var ano = livro.getElementsByTagName('ano')[0];
+
+var divLivro = document.getElementById("livro-especifico");
+
+var p = document.createElement("p");
+
+var texto = "ID: " + id + ", Título: " + titulo.childNodes[0].nodeValue + ", Autor: " + autor.childNodes[0].nodeValue + ", Ano: " + ano.childNodes[0].nodeValue;
+
+p.textContent = texto;
+divLivro.appendChild(p);
