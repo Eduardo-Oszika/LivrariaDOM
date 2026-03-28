9fde6d7529fe833134fc4d70c9acef0f20203441
Eduardo-Ozika <eduardoozika.ti@gmail.com>
2026-03-28 01:09:39 -0300

    pagina html com os titulos dos livros

---
+++ titulos.js
@@ -0,0 +1,20 @@
+
+var livrosXML = new XMLHttpRequest();
+livrosXML.open("GET", "livros.xml", false);
+livrosXML.send();
+
+var DOM = livrosXML.responseXML;
+
+var titulos = DOM.getElementsByTagName("titulo");
+
+var divTitulos = document.getElementById("titulos");
+
+console.log(titulos);
+
+for (var i = 0; i < titulos.length; i++) {
+    var titulo = titulos[i].childNodes[0].nodeValue;
+    console.log(titulo);
+    var p = document.createElement("p");
+    p.textContent = titulo;
+    divTitulos.appendChild(p);
+}
